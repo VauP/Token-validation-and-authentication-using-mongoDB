894e960bb05aecf908867fda8582bfb901001102
Víctor <victor.adpg@gmail.com>
2021-11-09 22:28:38 -0300

    first commit

---
+++ src/app/controllers/projectController.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const authMiddleware = require('../middlewares/auth');
+
+router.use(authMiddleware);
+
+router.get('/', (req, res) => {
+    res.send({ ok: true, user: req.userId });
+});
+
+module.exports = app => app.use('/projects', router);
